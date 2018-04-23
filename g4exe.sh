bf43559d5bc59c4ed618ea4576d95612dd06c702
liziwei <liziwei@mail.ustc.edu.cn>
2018-04-23 17:43:37 +0800

    g4exe add output name line

--- g4exe.sh
+++ g4exe.sh
@@ -7,4 +7,5 @@ which geant4-config
 mkdir -p `dirname $3`
 ./CRTest $@
 NAME=$3
-root -b -q "MultiTiersOutputfun_SiPM.C(\"$NAME\")"
+echo $NAME > $PWD/name.log
+root -b -q "Outputfun_MCP.C(\"$NAME\")"
