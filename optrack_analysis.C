050d292af767543ed2ba2c5f982493ab7fe7956f
liziwei <liziwei@mail.ustc.edu.cn>
2018-05-11 11:07:31 +0800

    optrack Modify: Correct GlobalTime caculation error!

--- optrack_analysis.C
+++ optrack_analysis.C
@@ -10,15 +10,16 @@ void optrack_analysis()
 {
     void DrawMyGraph(TGraph *datagraph, char *xtitle, char *ytitle, float MSize=1, int MStyle =28, int MColor=1, int LColor=1, float LWidth=1, int LStyle=1, int FColor=16);
     TFile *f1 = new TFile("CRTest.root","READ");
-	TTree *t1 = (TTree*)f1->Get("Run");
+    TTree *t1 = (TTree*)f1->Get("Run");
 
 
     int counts = 0;
 
     std::vector<int>* fID;
+    std::vector<double>* fGt;
     std::vector<double>* fL;
-	std::vector<int>* fBounce;
-	std::vector<double>* fWaveL;
+    std::vector<int>* fBounce;
+    std::vector<double>* fWaveL;
 
 
     std::vector<double>* ftR;
@@ -26,26 +27,28 @@ void optrack_analysis()
     std::vector<int>* fIDR;
     std::vector<int>* fIDL;
 
-	fID = new std::vector<int>;
-	fL = new std::vector<double>;
-	fWaveL = new std::vector<double>;
-	fBounce = new std::vector<int>;
+    fID = new std::vector<int>;
+    fGt = new std::vector<double>;
+    fL = new std::vector<double>;
+    fWaveL = new std::vector<double>;
+    fBounce = new std::vector<int>;
     ftR = new std::vector<double>;
-	ftL = new std::vector<double>;
-	fIDL = new std::vector<int>;
-	fIDR = new std::vector<int>;
+    ftL = new std::vector<double>;
+    fIDL = new std::vector<int>;
+    fIDR = new std::vector<int>;
 
 
     t1->SetBranchAddress("PmtR.t",&ftR);
     t1->SetBranchAddress("PmtL.t",&ftL);
     t1->SetBranchAddress("PmtR.trackID",&fIDR);
-	t1->SetBranchAddress("PmtL.trackID",&fIDL);
-	t1->SetBranchAddress("op.L",&fL);
+    t1->SetBranchAddress("PmtL.trackID",&fIDL);
+    t1->SetBranchAddress("op.Gt",&fGt);
+    t1->SetBranchAddress("op.L",&fL);
     t1->SetBranchAddress("op.WaveL",&fWaveL);
     t1->SetBranchAddress("op.ID",&fID);
-	t1->SetBranchAddress("op.Bounce",&fBounce);
-    
-    t1->SetBranchAddress("op.scint",&counts);
+    t1->SetBranchAddress("op.Bounce",&fBounce);
+
+    t1->SetBranchAddress("op.crkov",&counts);
 
     int N = 0;
 
@@ -59,10 +62,10 @@ void optrack_analysis()
 
     //binN = ID+1;
     //TH1D *HB = new TH1D;
-    
+
     //TH1D *HL = new TH1D("HL","length",200,0,0.4);
 
-    
+
 
     //t1->Draw("op.L>>HL","op.ID==binN");
 
@@ -70,11 +73,11 @@ void optrack_analysis()
 
     t1->GetEntry(0);
     N = counts;
-    
+
     cout<<"the photon quantity is "<<N<<endl;
 
     TH1D *HB = new TH1D("HB","bounce",N+1,0,N+1);
-    
+
     t1->Draw("op.Bounce>>HB");
 
     for(int i=0; i<N; i++){
@@ -85,31 +88,33 @@ void optrack_analysis()
         //return;
         //continue;
         /*
-        temp=fBounce->size();
-        for(int j=0;j<temp;j++){
-            if((*fBounce)[j]==ID) B++;
-        }
-        */
+           temp=fBounce->size();
+           for(int j=0;j<temp;j++){
+           if((*fBounce)[j]==ID) B++;
+           }
+         */
         temp=fL->size();
         flag = 1;
         L = 0;
+        t = 0;
         for(int k = 0;k<temp;k++)
         {
             if((*fID)[k]==ID) 
             {
                 if(flag)    {wvL=(*fWaveL)[k];flag=0;}
+                if (t <= (*fGt)[k]) t = (*fGt)[k];
                 L+=(*fL)[k];
             }
         }
-        
+
         detected = 0;
-        t = L*1.58/3e8/100*1e9;
+        //t = L*1.58/3e8/100*1e9;
 
         temp=fIDL->size();
         for(int s=0;s<temp;s++){
             if ((*fIDL)[s]==ID) {
                 detected =1;
-                t = (*ftL)[s];
+                //t = (*ftL)[s];
                 //cout<<"hit time = "<<t<<endl;
             }
         }
@@ -118,17 +123,17 @@ void optrack_analysis()
         for(int q=0;q<temp;q++){
             if ((*fIDR)[q]==ID) {
                 detected =2;
-                t = (*ftR)[q];
+                //t = (*ftR)[q];
             }
         }
-    outputdata<<ID<<"\t"<<wvL<<"\t"<<B<<"\t"<<L<<"\t"<<t<<"\t"<<detected<<endl;
+        outputdata<<ID<<"\t"<<wvL<<"\t"<<B<<"\t"<<L<<"\t"<<t<<"\t"<<detected<<endl;
 
 
-        
+
     }
 
-    
-	
-   
+
+
+
 
 }
--- optrack_buildtree.C
+++ optrack_buildtree.C
@@ -9,14 +9,14 @@
 
 void optrack_buildtree(){
     gStyle->SetOptStat(1111);
- 
+
     void DrawMyGraph(TGraph *datagraph, const char *xtitle, const char *ytitle, Float_t MSize=1, Int_t MStyle =28, Int_t MColor=1, Int_t LColor=1, Float_t LWidth=1, Int_t LStyle=1, Int_t FColor=16);
     void DrawMyHist1(TH1 *datahist, const char *xtitle,const char *ytitle, Color_t LColor=1, Width_t LWidth=3, Color_t TitleColor=1);
     void DrawMyHist2(TH2 *datahist, const char *xtitle,const char *ytitle, Color_t LColor=1, Width_t LWidth=3, Color_t TitleColor=1);
     void SetMyPad(TPad *pad,float left, float right, float top, float bottom);
     TLegend* DrawMyLeg(Double_t xlow=0.2, Double_t ylow=0.2, Double_t xup=0.5, Double_t yup=0.5, Int_t textFont=62, Size_t textSize=0.05);
-    
-    
+
+
     Int_t ID = 0,B = 0,binN = 0;
     Double_t L = 0;// path length (cm)
     Double_t t = 0; // hit time (ns)
@@ -26,13 +26,13 @@ void optrack_buildtree(){
     Int_t detected = 0; //val=0,undetected; val=1,detected by L; val=2,detected by R;
 
     Int_t counter1 = 0,counter2 = 0;
-    
+
     /*
-    Double_t x1[300]={0},x2[300]={0};
-    Double_t yB1[300]={0},yB2[300]={0};
-    Double_t yL1[300]={0},yL2[300]={0};
-    Double_t yW1[300]={0},yW2[300]={0};
-    Double_t yT1[300]={0},yT2[300]={0};
+       Double_t x1[300]={0},x2[300]={0};
+       Double_t yB1[300]={0},yB2[300]={0};
+       Double_t yL1[300]={0},yL2[300]={0};
+       Double_t yW1[300]={0},yW2[300]={0};
+       Double_t yT1[300]={0},yT2[300]={0};
      */
 
     TFile *f1 = new TFile("optrack.root","RECREATE");
@@ -55,21 +55,23 @@ void optrack_buildtree(){
 
     TH1D *HB1 = new TH1D("B_abs","",nID,0,nID);
     TH1D *HB2 = new TH1D("B_hit","",nID,0,nID);
-    
+
     TH1D *HL1 = new TH1D("L_abs","",nID,0,nID);
     TH1D *HL2 = new TH1D("L_hit","",nID,0,nID);
-    
+
     TH1D *HW1 = new TH1D("wvl_abs","",200,100,9e3);
     TH1D *HW2 = new TH1D("wvl_hit","",200,100,9e3);
-    
-    TH1D *HT1 = new TH1D("T_abs","",4e3,0,2e3);
-    TH1D *HT2 = new TH1D("T_hit","",4e3,0,2e3);
 
-    TH2D *H1 = new TH2D("Absorbed","",nID,0,nID,800,100,900);
-    TH2D *H2 = new TH2D("hitpmt","",nID,0,nID,800,100,900);
+    TH1D *HT1 = new TH1D("T_abs","",2e2,0,3);
+    TH1D *HT2 = new TH1D("Detected","",2e2,0,3);
+    TH1D *HT = new TH1D("Total","",2e2,0,3);
 
-    DrawMyHist1(HT1,"Time (ns)","Counts",1,2);
+    TH2D *H1 = new TH2D("Absorbed","",2e2,0,3,500,150,650);
+    TH2D *H2 = new TH2D("hitpmt","",2e2,0,3,500,150,650);
+
+    DrawMyHist1(HT1,"Time (ns)","Counts",4,2);
     DrawMyHist1(HT2,"Time (ns)","Counts",2,2);
+    DrawMyHist1(HT,"Time (ns)","Counts",1,2);
 
     DrawMyHist1(HL1,"path length (cm)","Counts",1,2);
     DrawMyHist1(HL2,"path length (cm)","Counts",2,2);
@@ -83,57 +85,58 @@ void optrack_buildtree(){
     DrawMyHist2(H1,"time (ns)","wavelength (nm)",1,2);
     DrawMyHist2(H2,"time (ns)","wavelength (nm)",2,2);
     H1->SetMarkerSize( 1 );
-    H1->SetMarkerStyle( 28 );
-    H1->SetMarkerColor( 1 );
+    H1->SetMarkerStyle( 4 );
+    H1->SetMarkerColor( kGray+3 );
 
     H2->SetMarkerSize( 1 );
-    H2->SetMarkerStyle( 26 );
+    H2->SetMarkerStyle( 8 );
     H2->SetMarkerColor( 2 );
 
     ifstream indata;
     indata.open("PhotonsInform.dat");
     while(indata&&(!indata.eof())){
-       
-		indata>>ID>>wvL>>B>>L>>t>>detected;
 
-		
-         // undetected 
+        indata>>ID>>wvL>>B>>L>>t>>detected;
+
+        HT->Fill(t);
+        // undetected 
         if (detected==0) {
-        /*
-        x1[counter1]=ID;
-        yB1[counter1]=B;
-        yL1[counter1]=L;
-        yW1[counter1]=wvL;
-        yT1[counter1]=t;
-        */
-        HB1->Fill(B);
-        HL1->Fill(L);
-        HW1->Fill(wvL);
-        HT1->Fill(t);
-        H1->Fill(t,wvL);
-        t1->Fill();
-        //cout<<x1[counter1]<<endl;
-        //cout<<yB1[counter1]<<endl;
-        counter1++;
+            /*
+               x1[counter1]=ID;
+               yB1[counter1]=B;
+               yL1[counter1]=L;
+               yW1[counter1]=wvL;
+               yT1[counter1]=t;
+             */
+            HB1->Fill(B);
+            HL1->Fill(L);
+            HW1->Fill(wvL);
+            HT1->Fill(t);
+            H1->Fill(t,wvL);
+            t1->Fill();
+            
+            //cout<<x1[counter1]<<endl;
+            //cout<<yB1[counter1]<<endl;
+            counter1++;
         }
         else {
-        /*
-        x2[counter2]=ID;
-        yB2[counter2]=B;
-        yL2[counter2]=L;
-        yW2[counter2]=wvL;
-        yT2[counter2]=t;
-        */
+            /*
+               x2[counter2]=ID;
+               yB2[counter2]=B;
+               yL2[counter2]=L;
+               yW2[counter2]=wvL;
+               yT2[counter2]=t;
+             */
 
-        HB2->Fill(B);
-        HL2->Fill(L);
-        HW2->Fill(wvL);
-        HT2->Fill(t);
-        H2->Fill(t,wvL);
-        t2->Fill();
-        counter2++;
+            HB2->Fill(B);
+            HL2->Fill(L);
+            HW2->Fill(wvL);
+            HT2->Fill(t);
+            H2->Fill(t,wvL);
+            t2->Fill();
+            counter2++;
 
-         }
+        }
     }
 
     t1->Write();
@@ -146,164 +149,187 @@ void optrack_buildtree(){
     cout<<" undetected = "<<counter1<<endl;
 
     indata.close();
-    
+
     /*
-    TGraph *gB1 = new TGraph(100,x1,yB1);
-    
-    TGraph *gL1 = new TGraph(counter1,x1,yL1);
-    TGraph *gW1 = new TGraph(counter1,x1,yW1);
-    TGraph *gT1 = new TGraph(counter1,x1,yT1);
+       TGraph *gB1 = new TGraph(100,x1,yB1);
 
-    TGraph *gB2 = new TGraph(counter2,x2,yB2);
-    TGraph *gL2 = new TGraph(counter2,x2,yL2);
-    TGraph *gW2 = new TGraph(counter2,x2,yW2);
-    TGraph *gT2 = new TGraph(counter2,x2,yT2);
+       TGraph *gL1 = new TGraph(counter1,x1,yL1);
+       TGraph *gW1 = new TGraph(counter1,x1,yW1);
+       TGraph *gT1 = new TGraph(counter1,x1,yT1);
 
-    DrawMyGraph(gT1,"ID", "Time", 1,28, 1,1, 1,1);
-    DrawMyGraph(gT2,"ID", "Time", 1,26, 2,2, 1,1);
+       TGraph *gB2 = new TGraph(counter2,x2,yB2);
+       TGraph *gL2 = new TGraph(counter2,x2,yL2);
+       TGraph *gW2 = new TGraph(counter2,x2,yW2);
+       TGraph *gT2 = new TGraph(counter2,x2,yT2);
 
-    TCanvas *c = new TCanvas("c","",1000,600);
-    c->cd();
-    gPad->SetLogy();
-    gT1->Draw("AP");
-    gT1->GetYaxis()->SetRangeUser(0,100);
-    
-    gB1->
-    gT2->Draw("Psame");
-    */
+       DrawMyGraph(gT1,"ID", "Time", 1,28, 1,1, 1,1);
+       DrawMyGraph(gT2,"ID", "Time", 1,26, 2,2, 1,1);
 
+       TCanvas *c = new TCanvas("c","",1000,600);
+       c->cd();
+       gPad->SetLogy();
+       gT1->Draw("AP");
+       gT1->GetYaxis()->SetRangeUser(0,100);
+
+       gB1->
+       gT2->Draw("Psame");
+     */
+    TCanvas *c1 = new TCanvas("c1","",800,600);
+    SetMyPad(c1, 0.12, 0.1, 0.1, 0.15);
     TCanvas *c2 = new TCanvas("c2","",1000,600);
-    SetMyPad(c2,0.1, 0.1,0.1, 0.15);
+    SetMyPad(c2, 0.12, 0.1, 0.1, 0.15);
+    c1->cd();
+    HT->Draw();
+    //HT1->Draw("same");
+    HT->SetLineStyle(7);
+    HT2->Draw("SAMES");
+    TLegend* leg1; 
+    //leg1 = DrawMyLeg(0.12,0.75,0.35,0.88);
+    leg1 = DrawMyLeg(0.6,0.2,0.75,0.32);
+    leg1->AddEntry(HT2,"Detected","lp");
+    leg1->AddEntry(HT,"Total","lp");
+    leg1->Draw();
+
+    gPad->Update();
+    TPaveStats *stL1 = (TPaveStats*)HT2->FindObject("stats");
+    //stR->SetName("PMT_Right");
+    stL1->SetY1NDC(0.58);
+    stL1->SetY2NDC(0.75);
+    //gPad->Modified();
+    //gPad->Update();
+    c1->SaveAs("T.png");
+
     c2->cd();
-    c2->SetLogx();
+    //c2->SetLogx();
     H2->Draw();
+    H2->GetXaxis()->SetRangeUser(0,10);
     H1->Draw("SAMES");
     TGaxis::SetMaxDigits(3);
     TLegend* leg; 
-	leg = DrawMyLeg(0.6,0.2,0.75,0.32);
-	leg->AddEntry(H2,"Hit PMT","lp");
-	leg->AddEntry(H1,"Absorbed","lp");
-	leg->Draw();
+    leg = DrawMyLeg(0.12,0.75,0.35,0.88);
+    leg->AddEntry(H2,"Detected","lp");
+    leg->AddEntry(H1,"Absorbed","lp");
+    leg->Draw();
 
     gPad->Update(); // inevitable!!
     TPaveStats *stL = (TPaveStats*)H2->FindObject("stats");
-	//stR->SetName("PMT_Right");
-	stL->SetY1NDC(0.4);
-	stL->SetY2NDC(0.68);
-	//gPad->Modified();
-	//gPad->Update();
-  c2->SaveAs("TvsWvL.png");
-    
+    //stR->SetName("PMT_Right");
+    stL->SetY1NDC(0.4);
+    stL->SetY2NDC(0.68);
+    //gPad->Modified();
+    //gPad->Update();
+    c2->SaveAs("TvsWvL.png");
+
 }
 void DrawMyGraph(TGraph *datagraph, const char *xtitle, const char *ytitle, Float_t MSize=1, Int_t MStyle =28, Int_t MColor=1, Int_t LColor=1, Float_t LWidth=1, Int_t LStyle=1, Int_t FColor=16){
-     datagraph->SetLineColor( LColor );
-     datagraph->SetLineWidth( LWidth );
-     datagraph->SetLineStyle( LStyle );
-     datagraph->SetMarkerSize( MSize );
-     datagraph->SetMarkerStyle( MStyle );
-     datagraph->SetMarkerColor( MColor );
-     datagraph->SetFillColor( FColor );
-     //datagraph->SetFillStyle( FStyle );
-     datagraph->GetXaxis()->SetTitle( xtitle);
-     datagraph->GetYaxis()->SetTitle( ytitle);
-     datagraph->GetXaxis()->SetAxisColor(1);
-     datagraph->GetYaxis()->SetAxisColor(1);
-     datagraph->GetXaxis()->SetLabelColor(1);
-     datagraph->GetYaxis()->SetLabelColor(1);
-     datagraph->GetXaxis()->SetLabelFont( 42 );
-     datagraph->GetYaxis()->SetLabelFont( 42 );
-     datagraph->GetXaxis()->SetLabelSize( 0.05 );
-     datagraph->GetYaxis()->SetLabelSize( 0.05 );
-     datagraph->GetXaxis()->SetLabelOffset( 0.01 );
-     datagraph->GetYaxis()->SetLabelOffset( 0.01 );
-     datagraph->GetXaxis()->SetTitleFont( 42 );
-     datagraph->GetYaxis()->SetTitleFont( 42 );
-     //datagraph->GetXaxis()->SetTitleColor( TitleColor);
-     //datagraph->GetYaxis()->SetTitleColor( TitleColor );
-     datagraph->GetXaxis()->SetTitleSize(0.06);
-     datagraph->GetYaxis()->SetTitleSize(0.06);
-     datagraph->GetXaxis()->SetTitleOffset(0.8);
-     datagraph->GetYaxis()->SetTitleOffset(0.8);
+    datagraph->SetLineColor( LColor );
+    datagraph->SetLineWidth( LWidth );
+    datagraph->SetLineStyle( LStyle );
+    datagraph->SetMarkerSize( MSize );
+    datagraph->SetMarkerStyle( MStyle );
+    datagraph->SetMarkerColor( MColor );
+    datagraph->SetFillColor( FColor );
+    //datagraph->SetFillStyle( FStyle );
+    datagraph->GetXaxis()->SetTitle( xtitle);
+    datagraph->GetYaxis()->SetTitle( ytitle);
+    datagraph->GetXaxis()->SetAxisColor(1);
+    datagraph->GetYaxis()->SetAxisColor(1);
+    datagraph->GetXaxis()->SetLabelColor(1);
+    datagraph->GetYaxis()->SetLabelColor(1);
+    datagraph->GetXaxis()->SetLabelFont( 42 );
+    datagraph->GetYaxis()->SetLabelFont( 42 );
+    datagraph->GetXaxis()->SetLabelSize( 0.05 );
+    datagraph->GetYaxis()->SetLabelSize( 0.05 );
+    datagraph->GetXaxis()->SetLabelOffset( 0.01 );
+    datagraph->GetYaxis()->SetLabelOffset( 0.01 );
+    datagraph->GetXaxis()->SetTitleFont( 42 );
+    datagraph->GetYaxis()->SetTitleFont( 42 );
+    //datagraph->GetXaxis()->SetTitleColor( TitleColor);
+    //datagraph->GetYaxis()->SetTitleColor( TitleColor );
+    datagraph->GetXaxis()->SetTitleSize(0.06);
+    datagraph->GetYaxis()->SetTitleSize(0.06);
+    datagraph->GetXaxis()->SetTitleOffset(0.8);
+    datagraph->GetYaxis()->SetTitleOffset(0.8);
 }
 
 void DrawMyHist1(TH1 *datahist, const char *xtitle,const char *ytitle, Color_t LColor=1, Width_t LWidth=3, Color_t TitleColor=1){
-     datahist->SetLineColor( LColor );
-     datahist->SetLineWidth( LWidth );
-     datahist->GetXaxis()->SetTitle( xtitle);
-     datahist->GetYaxis()->SetTitle( ytitle);
-     datahist->GetXaxis()->SetAxisColor(1);
-     datahist->GetYaxis()->SetAxisColor(1);
-     datahist->GetXaxis()->SetLabelColor(1);
-     datahist->GetYaxis()->SetLabelColor(1);
-     datahist->GetXaxis()->SetLabelFont( 42 );
-     datahist->GetYaxis()->SetLabelFont( 42 );
-     datahist->GetXaxis()->SetLabelSize( 0.06 );
-     datahist->GetYaxis()->SetLabelSize( 0.06 );
-     datahist->GetXaxis()->SetLabelOffset( 0.01 );
-     datahist->GetYaxis()->SetLabelOffset( 0.01 );
-     datahist->GetXaxis()->SetTitleFont( 42 );
-     datahist->GetYaxis()->SetTitleFont( 42 );
-     datahist->GetXaxis()->SetTitleColor( TitleColor);
-     datahist->GetYaxis()->SetTitleColor( TitleColor );
-     datahist->GetXaxis()->SetTitleSize(0.06);
-     datahist->GetYaxis()->SetTitleSize(0.06);
-     datahist->GetXaxis()->SetTitleOffset(1.0);
-     datahist->GetYaxis()->SetTitleOffset(0.8);
-     //datahist->GetXaxis()->SetBorderSize(5);
-     datahist->GetXaxis()->SetNdivisions(510);     
-     datahist->GetYaxis()->SetNdivisions(510);
-     datahist->GetXaxis()->CenterTitle();
-     datahist->GetYaxis()->CenterTitle();
+    datahist->SetLineColor( LColor );
+    datahist->SetLineWidth( LWidth );
+    datahist->GetXaxis()->SetTitle( xtitle);
+    datahist->GetYaxis()->SetTitle( ytitle);
+    datahist->GetXaxis()->SetAxisColor(1);
+    datahist->GetYaxis()->SetAxisColor(1);
+    datahist->GetXaxis()->SetLabelColor(1);
+    datahist->GetYaxis()->SetLabelColor(1);
+    datahist->GetXaxis()->SetLabelFont( 42 );
+    datahist->GetYaxis()->SetLabelFont( 42 );
+    datahist->GetXaxis()->SetLabelSize( 0.06 );
+    datahist->GetYaxis()->SetLabelSize( 0.06 );
+    datahist->GetXaxis()->SetLabelOffset( 0.01 );
+    datahist->GetYaxis()->SetLabelOffset( 0.01 );
+    datahist->GetXaxis()->SetTitleFont( 42 );
+    datahist->GetYaxis()->SetTitleFont( 42 );
+    datahist->GetXaxis()->SetTitleColor( TitleColor);
+    datahist->GetYaxis()->SetTitleColor( TitleColor );
+    datahist->GetXaxis()->SetTitleSize(0.06);
+    datahist->GetYaxis()->SetTitleSize(0.06);
+    datahist->GetXaxis()->SetTitleOffset(1.0);
+    datahist->GetYaxis()->SetTitleOffset(1.0);
+    //datahist->GetXaxis()->SetBorderSize(5);
+    datahist->GetXaxis()->SetNdivisions(510);     
+    datahist->GetYaxis()->SetNdivisions(510);
+    datahist->GetXaxis()->CenterTitle();
+    datahist->GetYaxis()->CenterTitle();
 }
 void DrawMyHist2(TH2 *datahist, const char *xtitle,const char *ytitle, Color_t LColor=1, Width_t LWidth=3, Color_t TitleColor=1){
-     datahist->SetLineColor( LColor );
-     datahist->SetLineWidth( LWidth );
-     datahist->GetXaxis()->SetTitle( xtitle);
-     datahist->GetYaxis()->SetTitle( ytitle);
-     datahist->GetXaxis()->SetAxisColor(1);
-     datahist->GetYaxis()->SetAxisColor(1);
-     datahist->GetXaxis()->SetLabelColor(1);
-     datahist->GetYaxis()->SetLabelColor(1);
-     datahist->GetXaxis()->SetLabelFont( 42 );
-     datahist->GetYaxis()->SetLabelFont( 42 );
-     datahist->GetXaxis()->SetLabelSize( 0.06 );
-     datahist->GetYaxis()->SetLabelSize( 0.06 );
-     datahist->GetXaxis()->SetLabelOffset( 0.01 );
-     datahist->GetYaxis()->SetLabelOffset( 0.01 );
-     datahist->GetXaxis()->SetTitleFont( 42 );
-     datahist->GetYaxis()->SetTitleFont( 42 );
-     datahist->GetXaxis()->SetTitleColor( TitleColor);
-     datahist->GetYaxis()->SetTitleColor( TitleColor );
-     datahist->GetXaxis()->SetTitleSize(0.06);
-     datahist->GetYaxis()->SetTitleSize(0.06);
-     datahist->GetXaxis()->SetTitleOffset(1.0);
-     datahist->GetYaxis()->SetTitleOffset(0.8);
-     //datahist->GetXaxis()->SetBorderSize(5);
-     datahist->GetXaxis()->SetNdivisions(510);     
-     datahist->GetYaxis()->SetNdivisions(510);
-     datahist->GetXaxis()->CenterTitle();
-     datahist->GetYaxis()->CenterTitle();
+    datahist->SetLineColor( LColor );
+    datahist->SetLineWidth( LWidth );
+    datahist->GetXaxis()->SetTitle( xtitle);
+    datahist->GetYaxis()->SetTitle( ytitle);
+    datahist->GetXaxis()->SetAxisColor(1);
+    datahist->GetYaxis()->SetAxisColor(1);
+    datahist->GetXaxis()->SetLabelColor(1);
+    datahist->GetYaxis()->SetLabelColor(1);
+    datahist->GetXaxis()->SetLabelFont( 42 );
+    datahist->GetYaxis()->SetLabelFont( 42 );
+    datahist->GetXaxis()->SetLabelSize( 0.06 );
+    datahist->GetYaxis()->SetLabelSize( 0.06 );
+    datahist->GetXaxis()->SetLabelOffset( 0.01 );
+    datahist->GetYaxis()->SetLabelOffset( 0.01 );
+    datahist->GetXaxis()->SetTitleFont( 42 );
+    datahist->GetYaxis()->SetTitleFont( 42 );
+    datahist->GetXaxis()->SetTitleColor( TitleColor);
+    datahist->GetYaxis()->SetTitleColor( TitleColor );
+    datahist->GetXaxis()->SetTitleSize(0.06);
+    datahist->GetYaxis()->SetTitleSize(0.06);
+    datahist->GetXaxis()->SetTitleOffset(1.0);
+    datahist->GetYaxis()->SetTitleOffset(1.0);
+    //datahist->GetXaxis()->SetBorderSize(5);
+    datahist->GetXaxis()->SetNdivisions(510);     
+    datahist->GetYaxis()->SetNdivisions(510);
+    datahist->GetXaxis()->CenterTitle();
+    datahist->GetYaxis()->CenterTitle();
 }
 void SetMyPad(TPad *pad,float left, float right, float top, float bottom){
-  pad->SetFillColor(10);
-  pad->SetBorderMode(0);
-  pad->SetBorderSize(0);
-  pad->SetFrameFillColor(10);
-  //pad->SetFrameFillStyle(3003);
-  pad->SetFrameBorderMode(0);
-  pad->SetFrameBorderSize(0);
-  pad->SetLeftMargin(left);
-  pad->SetRightMargin(right);
-  pad->SetTopMargin(top);
-  pad->SetBottomMargin(bottom);
+    pad->SetFillColor(10);
+    pad->SetBorderMode(0);
+    pad->SetBorderSize(0);
+    pad->SetFrameFillColor(10);
+    //pad->SetFrameFillStyle(3003);
+    pad->SetFrameBorderMode(0);
+    pad->SetFrameBorderSize(0);
+    pad->SetLeftMargin(left);
+    pad->SetRightMargin(right);
+    pad->SetTopMargin(top);
+    pad->SetBottomMargin(bottom);
 }
 TLegend* DrawMyLeg(Double_t xlow=0.2, Double_t ylow=0.2, Double_t xup=0.5, Double_t yup=0.5, Int_t textFont=62, Size_t textSize=0.05){
-  TLegend *leg = new TLegend(xlow,ylow,xup,yup);
-  leg->SetBorderSize(0);
-  leg->SetFillStyle(0);
-  leg->SetFillColor(10);
-  leg->SetTextFont(textFont);
-  leg->SetTextSize(textSize);
-  //leg->Draw("same");
-  return leg;
+    TLegend *leg = new TLegend(xlow,ylow,xup,yup);
+    leg->SetBorderSize(0);
+    leg->SetFillStyle(0);
+    leg->SetFillColor(10);
+    leg->SetTextFont(textFont);
+    leg->SetTextSize(textSize);
+    //leg->Draw("same");
+    return leg;
 }
