fe989f6a866ea3bf373e37c617bdd22d8038ba8e
liziwei <liziwei@mail.ustc.edu.cn>
2018-07-19 16:56:36 +0800

    improve OSC wv code

--- OSC_wv_analysis.C
+++ OSC_wv_analysis.C
@@ -14,8 +14,9 @@ void OSC_wv_analysis(){
     int v=0;
     char buff[1024];
     char name[1024];
-    const int n=4;
+    const int n=7;
     int npe[6]={5,10,12,20,30,40};
+    /*
     ofstream output("timeres.dat");
     for (int s=0;s<n;s++)
     {
@@ -30,12 +31,12 @@ void OSC_wv_analysis(){
 
         TH1F *h = new TH1F("h","",4000,-10,10);
         h->SetTitle("time resolution");
-        TCut cMCPy = "MCP1_global_maximum_y>0.002";
+        TCut cMCPy = "MCP1_global_maximum_y>0.0012";
         TCut cMCPQ = "MCP1_all_charge>0.02";
 
-        TCut cSiPMy = "MCP2_global_maximum_y>0.0015";
-        TCut cSiPMQ = "MCP2_all_charge>0.1";
-        TCut cSiPMQ_3 = "MCP2_all_charge>0.1";
+        TCut cSiPMy = "MCP2_global_maximum_y>0.001";
+        TCut cSiPMQ = "MCP2_all_charge>0.59";
+        TCut cSiPMQ_3 = "MCP2_all_charge>0.2";
 
 
         TCut cfail = "MCP1_twentypercent_failed!=1&&MCP2_twentypercent_failed!=1";
@@ -54,16 +55,17 @@ void OSC_wv_analysis(){
 
     }
     output.close();
+    */
     TCanvas *c2 = new TCanvas("c2","",800,600);
     c2->cd();
-    double vop[6];
-    double tr[6];
-    double x[4]={1.4,4.5,11,23};
+    double vop[n];
+    double tr[n];
+    double x[n]={2,7,10,18.3,36.7,59.9,115.6};
 
     //TH2F *gtr = new TH2F("gtr",";#V_{op} (V); TimeRes (ns)",7,54,61,200,0.3,1);
     ifstream input;
     input.open("timeres.dat");
-    for(int i=0;i<6;i++){
+    for(int i=0;i<n;i++){
         input>>vop[i]>>tr[i];
         cout<<vop<<"\t"<<tr<<endl;
     }
@@ -72,6 +74,7 @@ void OSC_wv_analysis(){
     gtr->SetMarkerSize(1.5);
     gtr->SetMarkerColor(2);
     gtr->Draw("AP");
+    //return;
     gtr->SetTitle("");
     gtr->GetXaxis()->SetTitle("npe");
     gtr->GetYaxis()->SetTitle("Time Res (ns)");
