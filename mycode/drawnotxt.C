fa7286e18c7635c510aee2ea2e31c80f8548bfea
liziwei <liziwei@mail.ustc.edu.cn>
2019-12-14 20:03:21 +0800

    save drawnotxt.C

--- mycode/drawnotxt.C
+++ mycode/drawnotxt.C
@@ -39,8 +39,16 @@ float y1[]={2.65,2.57,2.53,2.48,2.42,2.1,2.01,1.5,1.13,0.69,0.53,0.41,0.37,0.29,
 
 */
 //BJT_V3_BoardL-ch2,M=100
-float x1[]={10,12,15,18,20,23,25,28,30,33,35,38,40,43,45};
-float y1[]={3.46,3.26,2.95,2.63,2.43,2.00,1.61,0.99,0.68,0.43,0.36,0.27,0.23,0.18,0.16};
+
+float x1[]={20,50,100,112,140,200};
+float y1[]={154,153,141,139,151,152};
+float y1cor[]={116,111,107,107,111,116};
+
+/*
+float x2[]={4,7,12,17,21,28,35,42,49,70,};
+float y2[]={170,164,172,154,171,168,170,167,161};
+float y2col[]={141.2,139,142,140,137,146,146,140,131};
+*/
 /*
 //BJT_V3_BoardS-ch2,M=100
 float x1[]={15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100};
@@ -51,10 +59,8 @@ float y1[]={2.67,2.5,2.41,2.29,2.24,2.14,1.98,1.71,1.34,0.92,0.55,0.37,0.29,0.25
 
 
 int n = sizeof(x1)/sizeof(x1[0]);
-for(int i=0;i<n;i++){
-    x1[i]=x1[i]/100;
-}
 TGraph* g1 = new TGraph(n,x1,y1); 
+TGraph* g2 = new TGraph(n,x1,y1cor); 
 
 /*
 TH1F *h1 = new TH1F("h1","",100,-1,1);
@@ -65,14 +71,20 @@ TCanvas *c1 = new TCanvas("c1","",800,600);
 c1->cd();
 g1->Draw("AP");
 DrawMyfunc mydraw;
-mydraw.SetPad(c1,0.1,0.14,0.05,0.05);
+mydraw.SetPad(c1,0.12,0.14,0.05,0.05);
+g2->Draw("Psame");
 //mydraw.Graph(g1,"NPE","TimeRes (ps)",1.5,20,4);
 
 //mydraw.Graph(g1,"Work voltage (V)","Gain ",1.5,20,4);
 mydraw.Graph(g1,"Threshold (mV)","Dark Noise (s^{-1}) ",1.5,20,4);
 g1->GetYaxis()->SetTitleOffset(0.5);
-g1->GetYaxis()->SetRangeUser(-1,6);
-
+g1->GetYaxis()->SetRangeUser(98,190);
+mydraw.Graph(g1,"Threshold (mV)","Time res (ps) ",1.8,47,4);
+mydraw.Graph(g2,"Threshold (mV)","Time res (ps) ",1.5,20,2);
+TLegend *leg=mydraw.Leg(0.6,0.7,0.85,0.9);
+leg->AddEntry(g1,"no correction","lp");
+leg->AddEntry(g2,"with correction","lp");
+leg->Draw();
 /*
 mydraw.Graph(g[1],"","",1.2,23,2);
 mydraw.Graph(g[2],"","",1.2,21,1);
@@ -85,14 +97,13 @@ for(i=0;i<4;i++){
 if(i==0) g[i]->Draw("AP");
 else g[i]->Draw("Psame");
 sprintf(buff,"smear=%gps",sigma[3-i]*1e3);
-leg->AddEntry(g[3-i],buff,"lp");
 }
 //h1->Draw();
 leg->Draw();
 //TLatex *l=draw->Latex("this is a text");
 //l->Draw();
 */
-sprintf(buff,"%sBJT_V3_BoardL-ch2_M100.png",name);
+sprintf(buff,"threshold_TR.png",name);
 //
 c1->SaveAs(buff);
 
